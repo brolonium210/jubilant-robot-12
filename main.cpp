d5f3086bb522553dfa9b1a0ca280b2a99afb71b2
parrwgrah <parrgraham888@gmail.com>
2025-05-26 10:24:24 +1200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by parrwgrah on 26/05/25.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by parrwgrah on 26/05/25.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
